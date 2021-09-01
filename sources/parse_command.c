3f4421d48ce14d3ef7a4c8ee15592ec4acb6dc7d
Erchegyi David <edavid@2-F-5.42heilbronn.de>
2021-09-01 15:13:42 +0200

    add some layout to parse_command.c

--- sources/parse_command.c
+++ sources/parse_command.c
@@ -1,3 +1,24 @@
 // 1. expand those words that are not variable assignments and redirections
 // 2. redirections are performed
 // 3. expansions in each variable assignments
+
+
+// First order of operations
+//
+// 1.	Breaks input string into words and operators, obeying the Quoting rules.
+// 		These tokens (words/operators) are separated by metacharacters.
+//		metacharacters: ' ', '\t', '\n', '|', '&', ';', '(', ')', '<', '>'
+// 2.	Parses the tokens into simple and compound commands.
+// 3.	Performs the various shell expansions, breaking the expanded tokens into
+//		lists of filenames and commands and arguments.
+// 4.	Performs any necessary redirections and removes the redirection
+//		operators and their operands from the argument list.
+// 5.	Executes the command.
+// 6.	Optionally waits for the command to complete and collects its exit
+//		status.
+
+// Quoting rules:
+//
+// Escape character: '\'
+// Single quotes: '
+// Double quotes: "
