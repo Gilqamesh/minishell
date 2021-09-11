be821f84fed82ed15e0219a912675bebf8c5d2dd
Erchegyi David <edavid@2-G-3.42heilbronn.de>
2021-09-11 16:08:02 +0200

    quick norm fix

--- mylib/libft/ft_objlst_mapFromEnv.c
+++ mylib/libft/ft_objlst_mapFromEnv.c
@@ -6,7 +6,7 @@
 /*   By: edavid <edavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 17:49:39 by edavid            #+#    #+#             */
-/*   Updated: 2021/09/10 18:31:40 by edavid           ###   ########.fr       */
+/*   Updated: 2021/09/10 19:01:44 by edavid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ t_obj_lst	*ft_objlst_mapFromEnv(char **strArr)
 		value = ft_strchr(strArr[i], '=');
 		if (value)
 			value = ft_substr(value + 1, 0, ft_strlen(strArr[i])
-				- ft_strlen(key) - 1);
+					- ft_strlen(key) - 1);
 		ft_objlstadd_front(&lst, ft_objlst_new(key, value));
 	}
 	return (lst);
