346fa1ec459cc6ba3297cf1b91a650f2c38e5f2b
Erchegyi David <edavid@2-G-3.42heilbronn.de>
2021-09-12 21:16:26 +0200

    update executor.c with general observation on how a <command> works

--- sources/executor.c
+++ sources/executor.c
@@ -6,7 +6,7 @@
 /*   By: edavid <edavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 18:17:46 by edavid            #+#    #+#             */
-/*   Updated: 2021/09/12 21:01:47 by edavid           ###   ########.fr       */
+/*   Updated: 2021/09/12 21:15:58 by edavid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,8 @@ int	executor(t_minishell *mystruct)
 // wc -l < outfile 3     input redirection to wc
 // wc -l < outfile3 outfile3  '< outfile3' input redirection will get replaced by the arguments that follow
 // echo "hey" | wc -l < outfile3 outfile3   both the input from echo and '< outfile3' is replaced
+// GENERALLY?
+// echo "hey" | wc -l < outfile3 === ./pipex IN "echo hey" "wc -l" OUT  + ./pipex "outfile3" "wc -l" OUT
 // echo "hey" | cat < outfile3 outfile === cat outfile
 // echo "hey" | cat < outfile3 outfile outfile === cat outfile outfile
+// echo "hey" | echo "hi" | cat < outfile3
