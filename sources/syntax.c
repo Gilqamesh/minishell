a904fc711906fddf2fe0c302a293ef6082d8ad2d
Erchegyi David <edavid@2-G-3.42heilbronn.de>
2021-09-13 19:58:39 +0200

    update syntax.c

--- sources/syntax.c
+++ sources/syntax.c
@@ -14,7 +14,7 @@
 
 static int	parse_filename(char *str)
 {
-	if (isValidOperator(str) || isValidFilename(str) == true)
+	if (isValidOperator(str) == NULL && isValidFilename(str) == true)
 		return (0);
 	return (1);
 }
