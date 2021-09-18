57fde2b7c77c4ffcff0ce08da994d6f507739cd6
Erchegyi David <edavid@3-E-3.42heilbronn.de>
2021-09-18 20:41:31 +0200

    improve Makefiles

--- mylib/printf/ft_printf.h
+++ mylib/printf/ft_printf.h
@@ -6,13 +6,14 @@
 /*   By: edavid <edavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/26 14:18:21 by edavid            #+#    #+#             */
-/*   Updated: 2021/07/02 15:03:36 by edavid           ###   ########.fr       */
+/*   Updated: 2021/09/18 20:35:16 by edavid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include "../libft/libft.h"
 # include <stdarg.h>
 int		ft_printf(const char *format, ...);
 char	*malloc_conv_spec(char *format, int *format_index);
---
+++ mylib/printf/test.c
@@ -0,0 +1,7 @@
+#include "ft_printf.h"
+
+int main()
+{
+	ft_putstr_fd("hi", 1);
+	return (0);
+}
