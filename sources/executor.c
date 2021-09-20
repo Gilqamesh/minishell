df9b76050510c836394f6840b3e0b902bc5b4ae7
Erchegyi David <edavid@2-A-3.42heilbronn.de>
2021-09-20 17:47:37 +0200

    update executor.c

--- sources/executor.c
+++ sources/executor.c
@@ -6,7 +6,7 @@
 /*   By: edavid <edavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 18:17:46 by edavid            #+#    #+#             */
-/*   Updated: 2021/09/19 21:51:50 by edavid           ###   ########.fr       */
+/*   Updated: 2021/09/20 17:45:18 by edavid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,16 @@ int	executor(t_minishell *mystruct)
 {
 	t_list		*cur;
 
+	if (ft_simpleCmdsize(mystruct->nodes) == 1
+		&& isStrBuiltin(mystruct->nodes->arguments[0]))
+	{
+		executeBuiltin(mystruct, mystruct->nodes->arguments, false);
+		return (0);
+	}
 	cur = mystruct->pipeLines;
 	while (cur)
 	{
-		if (ft_simpleCmdsize(cur->content) == 1
-			&& isStrBuiltin(((t_simpleCmd *)cur->content)->arguments[0]))
-			executeBuiltin(mystruct, ((t_simpleCmd *)cur->content)->arguments,
-				false);
-		else
-			ft_pipex(mystruct, (t_simpleCmd *)cur->content);
+		ft_pipex(mystruct, (t_simpleCmd *)cur->content);
 		cur = cur->next;
 	}
 	return (0);
